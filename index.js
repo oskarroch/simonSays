0dafc5b8382617e3920f4ac6405d113eb009ae6e
Oskarrro03 <oskarrochowiak@gmail.com>
2022-10-27 13:52:05 +0200

    commit

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
-let randomNumber;
-let randomChosenColor;
+
 let gamePattern = [];
 
+let userClickedPattern = [];
+
+$(".btn").on("click", function () {
+  const userChosenColor = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColor);
+});
+
 function nextSequence() {
-  randomNumber = Math.floor(Math.random() * 4);
-  randomChosenColor = buttonColors[randomNumber];
+  const randomNumber = Math.floor(Math.random() * 4);
+
+  const randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
   $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+
   const audio = new Audio("sounds/" + randomChosenColor + ".mp3");
   audio.play();
 }
