fea79f430387f9ab42881e3cd2c138fc79908864
Oskarrro03 <oskarrochowiak@gmail.com>
2022-10-27 14:54:08 +0200

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
 
 $(".btn").on("click", function () {
   const userChosenColor = $(this).attr("id");
@@ -10,6 +11,7 @@ $(".btn").on("click", function () {
   setTimeout(() => {
     $(this).removeClass("pressed");
   }, 100);
+  checkAnswer();
 });
 
 function nextSequence() {
@@ -18,9 +20,39 @@ function nextSequence() {
   gamePattern.push(randomChosenColor);
   $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
   playSound(randomChosenColor);
+  $("#level-title").text(`Level ${level}`);
+  level++;
 }
 
 function playSound(name) {
   const audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+$(document).on("keypress", () => {
+  if (level === 0) {
+    nextSequence();
+  }
+});
+
+function checkAnswer() {
+  if (
+    userClickedPattern[userClickedPattern.length - 1] ===
+    gamePattern[userClickedPattern.length - 1]
+  ) {
+    if (userClickedPattern.length === gamePattern.length) {
+      userClickedPattern = [];
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    const wrong = new Audio("sounds/wrong.mp3");
+    wrong.play();
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+  }
 }
