fffe6925b4a604b286e45b0c4c7af3dd909d8361
Oskarrro03 <oskarrochowiak@gmail.com>
2022-10-27 13:14:23 +0200

    commit

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ function nextSequence() {
   randomNumber = Math.floor(Math.random() * 4);
   randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+  $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+  const audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
 }
